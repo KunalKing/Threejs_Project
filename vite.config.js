5b2aea08fff9cd79180f6248202f81d65aec0625
KunalKing <kunalprasad957@gmail.com>
2024-05-04 16:55:54 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['src/main.jsx']
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000
   }
-}
+})
 
